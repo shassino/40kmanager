e5b936774ba6fcebb46d5c46f3435e4f43035b10
Francesco Scavone <shassino@gmail.com>
2017-12-28 04:13:45 +0100

    drawing manually hexes and style it

---
+++ hexmap.js
@@ -0,0 +1,148 @@
+var width;
+var height;
+var numHexX;
+
+var topology;
+var projection;
+
+var path;
+
+var svg;
+
+var zoom;
+
+numHexX = 10;
+width = window.innerWidth;
+height = (width / numHexX / 1.55) * 2 * Math.sin(Math.PI / 3) * 4.5;
+
+var hexes = CalcHex(numHexX, width, height);
+
+svg = d3.select("#map")
+    .append("svg:svg")
+    .attr("id","svg_1")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("pointer-events", "all")
+    .call(zoom = d3.zoom().on("zoom", rescale))
+    .on("dblclick.zoom", null)
+    .append("svg:g");
+
+svg.append("svg:rect")
+    .attr("id","rect_1")
+    .attr("width", width)
+    .attr("heigh", height)
+    .attr("fill", 'none');
+
+svg.append("image")
+    .attr("xlink:href", "http://best5.it/b5/wp-content/uploads/2014/08/Gli-scoiattoli-il-genere-Tamias-1-800x400-800x400.jpg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("preserveAspectRatio", "none");
+
+svg.attr("class", "hexagon")
+    .selectAll("path")
+    .data(hexes.id)
+    .enter().append("path")
+    .attr("d", function(d) { return hexes.d[d]; })
+    .attr("class", "red")
+    .on("click", click);
+
+function CalcHex(numHexX, width, height) {
+    var radius = width / numHexX / 1.55;
+    var dy = radius * 2 * Math.sin(Math.PI / 3);
+    var dx = radius * 3;
+
+    var n = Math.floor(width / dx);
+    var m = Math.floor(width / dy);
+
+    var vertex = [
+        [0.5, -0.866],
+        [-0.5, -0.866],
+        [-1, 0],
+        [-0.5, 0.866],
+        [0.5, 0.866],
+        [1, 0]
+    ];
+
+    var vector = [];
+    var ids = [];
+    var count = 0;
+
+    for (var y = 0; y < m; y++){
+        for (var x = 0; x < n; x++){
+            var movX = x * dx + (y & 1) * dx / 2 + radius;
+            var movY = y * dy / 2 + dy / 2;
+
+            var points = " M " + (vertex[0][0] * radius + movX) + " " + (vertex[0][1] * radius + movY);
+            for (var i = 1; i < 6; i++){
+                points += " L " + (vertex[i][0] * radius + movX) + " " + (vertex[i][1] * radius + movY);
+            }
+            points += " L " + (vertex[0][0] * radius + movX) + " " + (vertex[0][1] * radius + movY);
+
+            vector.push(points);
+            ids.push([count++]);
+        }
+    }
+
+    return {
+        d: vector,
+        id: ids
+    };
+}
+
+
+window.onresize = function(){
+    width = window.innerWidth;
+    height = width / 16 * 9;
+    radius = width / 20;
+    
+    d3.select("#svg_1")
+        .attr("width", width)
+        .attr("height", height);
+    d3.select("#rect_1")
+        .attr("width", width)
+        .attr("height", height);
+}
+
+/**
+* rescale:
+* rescale main svg(vis)
+*
+* Global variables:
+* oldTrans
+*/
+function rescale(){
+    transform = d3.event.transform;
+
+    /* do not let the user to zoom out beyond initial scale */
+    if (transform.k < 1) {
+        transform.k = 1;
+        //zoom.transform(transform);
+    }
+
+    /* a scala 2, il transform e' a -((width * scala) - width) */
+    var limitX = -((width * transform.k) - width);
+    var limitY = -((height * transform.k) - height);
+
+    /* calc boundaries */
+    if (transform.x > 0){
+        transform.x = 0;
+    } else if (transform.x < limitX){
+        transform.x = limitX;
+    }
+
+    if (transform.y > 0){
+        transform.y = 0;
+    } else if (transform.y < limitY){
+        transform.y = limitY;
+    }
+
+    svg.attr("transform", transform);
+    //zoom.transform(transform);
+}
+
+function click(d) {
+    d3.select(this).classed("red", !d.fill);
+    d3.select(this).classed("blue", d.fill);
+    d.fill = !d.fill;
+}
