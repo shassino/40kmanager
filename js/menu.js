93ab11a68289ba1e649b8221d9862a4ad8427c3a
Francesco Scavone <shassino@gmail.com>
2018-01-18 13:12:33 +0100

    completed login handling

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,24 @@
+var sessionId = "";
+
 function OnLoadMenu() {
+    LoadLoginDropdown(); //Fill login dropdown
+};
+
+function LoadLoginDropdown(){
+    $("#loginDropdown").html(
+        '<form class="px-4 py-3" id="loginForm" method="post" action="login.php" role="form">'+
+            '<div class="form-group">'+
+                '<label for="InputUsername">Username</label>'+
+                '<input type="text" name="username" class="form-control" id="InputUsername" placeholder="Username">'+
+            '</div>'+
+            '<div class="form-group">'+
+                '<label for="InputPassword">Password</label>'+
+                '<input type="password" name="password" class="form-control" id="InputPassword" placeholder="Password">'+
+            '</div>'+
+            '<button type="submit" class="btn btn-dark">Confirm</button>'+
+        '</form>'
+    );
+
     $('#loginForm').submit(function(e){
         form = $('#loginForm :input');
         var values = {};
@@ -8,12 +28,35 @@ function OnLoadMenu() {
             }
         })
 
-        $.post("./php/login.php", JSON.stringify(values), function(data,status,xhr) {
-            //handle failure
-            console.log(status);
-            //do something on success
+        $.post("./php/login.php", JSON.stringify(values), function(data, status, xhr){
+            if (status == "error"){
+                //handle failure
+                console.log(status);
+                LoadLoginFailedDropdown();
+            }
+            else {
+                sessionId = data;
+                LoadLogoutDropdown(values["username"]);
+            }
         });
 
         e.preventDefault();
     });
-};
+}
+
+function LoadLoginFailedDropdown(){
+    LoadLoginDropdown();
+    $("#loginDropdown").append("<br>Username or password invalid");
+}
+
+function LoadLogoutDropdown(username){
+    $("#loginDropdown").html(
+        '<div style="padding-left: 10px;">'+
+            '<p>Logged as: ' + username + '</p>'+
+            '<button type="button" id="logoutButton" class="btn btn-dark">Logout</button>'+
+        '</div>');//do something on success
+
+    $("#logoutButton").click(function(){
+        alert("clicked logout");
+    })
+}
