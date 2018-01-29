64fce87120d6822312920b2e2d6de6e805bf4292
Francesco Scavone <shassino@gmail.com>
2018-01-29 16:18:56 +0100

    sql creator for new tables

--- db.sql
+++ db.sql
@@ -24,3 +24,39 @@ CREATE TABLE `levels` (
   PRIMARY KEY (`memberID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+CREATE TABLE `championship` (
+  `championshipID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `rules` longtext NOT NULL,
+  `active` bool NOT NULL,
+  PRIMARY KEY (`championshipID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `rounds` (
+  `roundID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `championship` varchar(255) NOT NULL,
+  PRIMARY KEY (`roundID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `matches` (
+  `matchID` int(11) NOT NULL AUTO_INCREMENT,
+  `p1` varchar(255) NOT NULL,
+  `p2` varchar(255) NOT NULL,
+  `round` varchar(255) NOT NULL,
+  `obj1` int(16) NOT NULL,
+  `obj2` int(16) NOT NULL,
+  `lost1` int(16) NOT NULL,
+  `lost2` int(16) NOT NULL,
+  `report` longtext NOT NULL,
+  `played` bool NOT NULL,
+  `expire` datetime NOT NULL,
+  PRIMARY KEY (`matchID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `army` (
+  `armyID` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) NOT NULL,
+  `list` longtext NOT NULL,
+  PRIMARY KEY (`armyID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
