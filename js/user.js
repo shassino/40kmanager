7fbe907620eee904fb504910bceef5e19280de64
Francesco Scavone <shassino@gmail.com>
2018-02-18 17:13:47 +0100

    user list shower

--- js/user.js
+++ js/user.js
@@ -1,3 +1,35 @@
 function userOnLoad(name){
-    
+    let request = {};
+    request.session = GetSessionId();
+    request.username = name;
+    request.operation = "getprofile";
+    RequestData("./php/users.php", request, function(response){
+        FillProfile(response);
+    });    
 }
+
+function FillProfile(response){
+    let html =
+        '<table class="table table-sm" style="width: 50%;">'+
+            '<tbody>'+
+                '<tr>'+
+                    '<th scope="row">Army name: </th>'+
+                    '<td>'+response.armyname+'</td>'+
+                '</tr>'+
+                '<tr>'+
+                    '<th scope="row">Faction: </th>'+
+                    '<td>'+response.faction+'</td>'+
+                '</tr>'+
+                '<tr>'+
+                    '<th scope="row">Updates remaining: </th>'+
+                    '<td>'+response.tokens+'</td>'+
+                '</tr>'+
+            '</tbody>'+
+        '</table>'+
+        '<label for="listText" class="col-form-label">Army list: </label>'+
+        '<div class="form-control" id="listText"></div>';
+    
+    $('#usrprofileDiv').html(html);
+    $('#listText').html(response.list);
+}
+//# sourceURL=./js/user.js
